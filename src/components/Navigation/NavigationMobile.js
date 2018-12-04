a79a282e7403f15eb1cdccd561ff8b5b34b3cac4
hirotachi <milesfiles0@gmail.com>
2018-12-04 15:09:30 +0100

    add nav styles mobile layout

--- src/components/Navigation/NavigationMobile.js
+++ src/components/Navigation/NavigationMobile.js
@@ -13,11 +13,49 @@ class NavigationMobile extends Component {
   };
 
   //Menu handlers===========================================
+  componentDidUpdate(){
+    const currentRoute = this.props.redirect.location.pathname;
+    if(currentRoute === "/support"){
+      const logo = document.querySelector(".nav__logo");
+      logo.style.opacity = 0;
+      const showLogo = setTimeout(() => {
+        logo.classList.add("slide_down-in");
+        clearTimeout(showLogo);
+      }, 500)
+    }
+  }
+
   showMenu = () => {
+    if (this.props.redirect.location.pathname === "/dashboard") { // if page is dashboard hide logo on menu open
+      const logo = document.querySelector(".nav__logo");
+      logo.classList.add("slide_up-out");
+      logo.classList.remove("slide_down-in");
+    }
     this.setState(() => ({showMenu: true}));
+    const showMenu = setTimeout(() => {
+      const menu = document.querySelector(".nav__menu");
+      menu.classList.add("slide_down-in");
+      clearTimeout(showMenu);
+    }, 100);
   };
-  hideMenu = () => {
-    this.setState(() => ({showMenu: false}));
+  hideMenu = () => { // if page is support keep logo
+    const menu = document.querySelector(".nav__menu");
+    menu.classList.add("slide_up-out");
+    menu.classList.remove("slide_down-in");
+    if (this.props.redirect.location.pathname === "/dashboard") {
+      const hideLogo = setTimeout(() => {
+        this.setState(() => ({showMenu: false}));
+        const logo = document.querySelector(".nav__logo");
+        logo.classList.remove("slide_up-out");
+        logo.classList.add("slide_down-in");
+        clearTimeout(hideLogo);
+      }, 300);
+    } else {
+      const hideMenu = setTimeout(() => {
+        this.setState(() => ({showMenu: false}));
+        clearTimeout(hideMenu);
+      }, 300);
+    }
   };
 
   //===========================================
@@ -36,37 +74,60 @@ class NavigationMobile extends Component {
   };
 
   goHome = () => {
-    this.props.redirect.history.push("/")
+    this.props.redirect.history.push("/");
+  };
+
+  redirect = () => {
+    this.hideMenu();
+    const currentRoute = this.props.redirect.location.pathname;
+    if( currentRoute === "/dashboard"){
+      this.props.redirect.history.push("/support");
+    }else if (currentRoute === "/support"){
+      const logo = document.querySelector(".nav__logo");
+      logo.classList.add("slide_up-out");
+      logo.classList.remove("slide_down-in");
+      const routeToDashboard = setTimeout(() => {
+        this.props.redirect.history.push("/dashboard");
+        clearTimeout(routeToDashboard);
+      }, 500)
+    }
   };
 
   render() {
     return (
-      <div>
+      <div className="nav">
         {
           this.props.auth && Object.values(this.props.controls).indexOf(true) === -1 &&
           this.props.redirect.location.pathname !== "/support" &&
           <SearchMobile/>
         }
-        <p onClick={this.goHome}>XAM</p>
+        <p className={`nav__logo slide_down-in logo${
+          this.props.redirect.location.pathname === "/support" ? "--sup" : "--dash"
+          }`}
+           onClick={this.goHome}>XAM</p>
         {
           this.state.showMenu ?
-            <div onClick={this.hideMenu}><CloseIcon/></div> :
-            <div onClick={this.showMenu}><MenuIcon/></div>
+            <div onClick={this.hideMenu}>
+              <CloseIcon style="nav__menu--btn"/>
+            </div> :
+            <div onClick={this.showMenu}>
+              <MenuIcon style="nav__menu--btn"/>
+            </div>
         }
         {
           this.state.showMenu &&
-          <div>
+          <div className="nav__menu">
             {
               this.props.auth &&
-              <button onClick={this.handleLogout}>Logout</button>
+              <button className="nav__menu--link" onClick={this.handleLogout}>Logout</button>
             }
             {
               this.props.redirect.location.pathname === "/dashboard" ?
-                <NavLink to="/support">support</NavLink> :
+                <button className="nav__menu--link" onClick={this.redirect}>support</button> :
                 <React.Fragment>
                   {
                     this.props.auth ?
-                      <NavLink to="/dashboard">Dashboard</NavLink> :
+                      <button className="nav__menu--link" onClick={this.redirect}>Dashboard</button> :
                       <div>
                         <button onClick={this.handleLogin}>Login</button>
                         <button onClick={this.handleSignUp}>Signup</button>
