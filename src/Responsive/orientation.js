2d2fe2d4ea104809494ac00979f29f270b6c4f1a
hirotachi <milesfiles0@gmail.com>
2019-01-01 05:16:58 +0100

    add babel polyfill for older browser and fix orientation func for all browsers

--- src/Responsive/orientation.js
+++ src/Responsive/orientation.js
@@ -1,51 +1,61 @@
+// export default (orientation) => {
+//   const screenWidth = screen.width;
+//   const browserInfo = navigator.userAgent;
+//   const {angle, type} = screen.orientation;
+//   //==========================================
+//   const mobileCheck = browserInfo.toLowerCase().includes("mobile");
+//   const widthCheck = screenWidth < 815;
+//   const tabletWidthCheck = screenWidth < 850 && screenWidth > 480;
+//   const deviceToggleCheck = browserInfo.toLowerCase().includes("nexus");
+//   const angleCheck = angle === 90;
+//   const landscapeCheck = type.toLowerCase().includes("landscape");
+//   const requestLandscape = !!orientation && orientation.toLowerCase() === "landscape";
+//
+//   //==========================================
+//   const mobileLandscape =
+//     requestLandscape &&
+//     landscapeCheck &&
+//     mobileCheck &&
+//     !deviceToggleCheck &&
+//     widthCheck &&
+//     angleCheck;
+//   const mobileDesktopLandscapeCheck =
+//     requestLandscape &&
+//     landscapeCheck &&
+//     mobileCheck &&
+//     deviceToggleCheck &&
+//     widthCheck &&
+//     angleCheck;
+//
+//   const tabletDesktopCheck =
+//     requestLandscape &&
+//     !landscapeCheck &&
+//     mobileCheck &&
+//     deviceToggleCheck &&
+//     tabletWidthCheck &&
+//     !angleCheck;
+//
+//
+//   if (!!orientation) {
+//     if (mobileLandscape) {
+//       return true;
+//     } else if (mobileDesktopLandscapeCheck) {
+//       return true;
+//     } else if (tabletDesktopCheck) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } else {
+//     return null;
+//   }
+//
+// };
+
 export default (orientation) => {
-  const screenWidth = screen.width;
-  const browserInfo = navigator.userAgent;
-  const { angle, type } = screen.orientation;
-  //==========================================
-  const mobileCheck = browserInfo.toLowerCase().includes("mobile");
-  const widthCheck = screenWidth < 815;
-  const tabletWidthCheck = screenWidth < 850 && screenWidth > 480;
-  const deviceToggleCheck = browserInfo.toLowerCase().includes("nexus");
-  const angleCheck = angle === 90;
-  const landscapeCheck = type.toLowerCase().includes("landscape");
-  const requestLandscape = !!orientation && orientation.toLowerCase() === "landscape";
-  //==========================================
-  const mobileLandscape =
-    requestLandscape &&
-    landscapeCheck &&
-    mobileCheck &&
-    !deviceToggleCheck &&
-    widthCheck &&
-    angleCheck;
-  const mobileDesktopLandscapeCheck =
-    requestLandscape &&
-    landscapeCheck &&
-    mobileCheck &&
-    deviceToggleCheck &&
-    widthCheck &&
-    angleCheck;
+  window.addEventListener("orientationchange", () => handleChange(orientation));
+}
 
-  const tabletDesktopCheck =
-    requestLandscape &&
-    !landscapeCheck &&
-    mobileCheck &&
-    deviceToggleCheck &&
-    tabletWidthCheck &&
-    !angleCheck;
-
-  if ( !!orientation ) {
-    if ( mobileLandscape ) {
-      return true;
-    } else if ( mobileDesktopLandscapeCheck ) {
-      return true;
-    } else if ( tabletDesktopCheck ) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return null;
-  }
-
+const handleChange = (orientation) => {
+  return window.matchMedia(`(orientation: ${orientation})`).matches;
 };
